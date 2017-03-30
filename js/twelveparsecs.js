fe259cb515583dbc14846f820a3830b57e5cec0a
Will Warren <will.warren04@gmail.com>
2017-03-29 20:57:02 -0400

    rolling on

--- js/twelveparsecs.js
+++ js/twelveparsecs.js
@@ -1,6 +1,7 @@
 //js file for star wars api hw
 
 $(document).ready(function() {
+
 $('#title').click(function(event) {
     console.log(event + "Clicked");
     $.ajax({
@@ -9,13 +10,12 @@ $('#title').click(function(event) {
 
         success: function(result) {
             $result = result;
-            console.log("Hiii ++ " + $result)
             $bigbox =$('<div>');
 
-            for(var i = 0; i < r; i++) {
+            for(var i = 0; i < 5; i++) {
                 $cat = $('<h4>');
-                $name =
-                $cat.append($result.planets.serializeArray);
+                console.log(result);
+                $cat.append($result);
                 $bigbox.append($cat);
             }
             $('#display').append($bigbox);
