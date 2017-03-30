ebc67d3b804539c699286de4bdcf4c2bcfd5c601
Will Warren <will.warren04@gmail.com>
2017-03-29 22:39:11 -0400

    names/titles being displayed

--- js/twelveparsecs.js
+++ js/twelveparsecs.js
@@ -2,22 +2,37 @@
 
 $(document).ready(function() {
 
-$('#title').click(function(event) {
-    console.log(event + "Clicked");
+
+//click button to search
+$('#getInfo').click(function(event) {
+    event.preventDefault();
+    //get category
+    let $val = $('#searchParams :input');
+    $val = $val.serialize();
+    let $choice = $val.slice(5) + '/';
+    console.log($choice);
+    //make ajax request
     $.ajax({
         type: "GET",
-        url: 'https://swapi.co/api/',
-
+        url: 'https://swapi.co/api/' + $choice,
+        // data: '{ results : name }',
         success: function(result) {
-            $result = result;
-            $bigbox =$('<div>');
+            let $result = result;
+            let $bigbox =$('<div>');
 
-            for(var i = 0; i < 5; i++) {
+            for(var i = 0; i < $result.results.length; i++) {
                 $cat = $('<h4>');
-                console.log(result);
-                $cat.append($result);
-                $bigbox.append($cat);
+                if($choice === "films/") {
+                    console.log($result.results[i].title);
+                    $cat.append($result.results[i].title);
+                    $bigbox.append($cat);
+                } else {
+                    console.log($result.results[i].name);
+                    $cat.append($result.results[i].name);
+                    $bigbox.append($cat);
+                }
             }
+            $('#display').empty();
             $('#display').append($bigbox);
 
 
