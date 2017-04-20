98189955a9af0ae2a1877c1b3c26afa162d19607
Will Warren <will.warren04@gmail.com>
2017-04-20 14:09:37 -0400

    only one listing printing per click, which is good. active class not turning on and off as I need to. maybe a toggle is a better solution that rm/add?

--- js/twelveparsecs.js
+++ js/twelveparsecs.js
@@ -1,47 +1,38 @@
 //js file for star wars api hw
 
 $(document).ready(function() {
-
+//need to add active class on click but take off previous active classes
+$('.display').click(function(event) {
+    $src = $(event.target);
+    console.log($src);
+    if ($('li').attr('class') == '.active') {
+        console.log("f1");
+        $(this).removeClass('active');
+        $src.addClass('active');
+    }});
 
 $('.display').click(function(event) {
-    event.preventDefault();
-    //get category from li's parent div
-    let $parent = $('li').parent();
-    let $cat = $parent.attr('class');
-    let cat = String($cat);
-    //get id to search for..right now id doesn't change if user clicks more than once.
-    let $id = $('li.active').attr('id');
-    //slice off num to use in ajax url req
-    if($id.length == 4) {
-        id_num = $id.slice(-1) + '/';
-    } else if ($id.length == 5) {
-        id_num = $id.slice(-2) + '/';
-    } else {
-        id_num = $id.slice(-3) + '/';
-    }
+    // get url from id...still having trouble clearing and picking new url each time
+    let $url = $('li.active').attr('id')
+    console.log($url);
+    $info = ($('<ul id="details">'))
 
-    $.ajax({         // do I need to create all the jquery objs in ajax or outside?
+    $.ajax({
         type: 'GET',
-        url: 'https://swapi.co/api/' + $cat + id_num,
+        url: $url,
         success: function(result) {
-            $info = $('<div>');
-            $info.append($('<ul id="details">'))
+
             $keys = Object.keys(result);
             $values = Object.values(result);
             for(var i = 0; i < $keys.length; i++) {
-                $info.append($keys[i] + ": \t" +  $values[i]);
-                $info.append($("<br>"))
+                $info.append("<li>" + $keys[i] + ": \t" +  $values[i] + '</li>');
             }
             console.log($info);
-
+            $('li.active').empty();
             $('li.active').append($info);
         }
     })
-
 });
-
-
-//good progress.. now make detais appear when click on the name h3
 
 //click button to search
 $('#getInfo').click(function(event) {
@@ -55,7 +46,6 @@ $('#getInfo').click(function(event) {
     $.ajax({
         type: "GET",
         url: 'https://swapi.co/api/' + $choice,
-        // data: '{ results : name }',
         success: function(result) {
             let $result = result
             let $bigbox =$('<div>');
@@ -64,7 +54,7 @@ $('#getInfo').click(function(event) {
                 $cat = $('<li>');
                 if($choice === "films/") {
                     $cat.append("Episode " + $result.results[i].episode_id + ':\t' + $result.results[i].title);
-                    $cat.attr("id", "id_" + (i+1));
+                    $cat.attr("id", "id_" + $result.results[i].url);
                     $($cat).on('click', function() {
                       $($cat).removeClass('active');
                       $(this).addClass('active');
@@ -73,23 +63,18 @@ $('#getInfo').click(function(event) {
                     $bigbox.append($cat);
                 } else {
                     $cat.append($result.results[i].name);
-                    $($cat).on('click',function() {
+                    $cat.attr("id", $result.results[i].url);
+                    $($cat).on('click', function() {
                       $($cat).removeClass('active');
                       $(this).addClass('active');
                     });
-                    $cat.attr("id", "id_" + (i+1));
                     $bigbox.attr("class", $choice)
                     $bigbox.append($cat);
                 }
-
             }
-
             $('.display').empty();
             $('.display').append($bigbox);
             }
 })
-
-
 });
-
 });
