f8b2d6b35d013250fd62572fbfe35cc222aff760
Will Warren <will.warren04@gmail.com>
2017-04-20 23:04:28 -0400

    all problems fixed, now I must only display the title/name for the url entries in the result

--- js/twelveparsecs.js
+++ js/twelveparsecs.js
@@ -2,14 +2,15 @@
 
 $(document).ready(function() {
 //need to add active class on click but take off previous active classes
+
 $('.display').click(function(event) {
     $src = $(event.target);
     console.log($src);
     if ($('li').is('.active')) {
-        console.log("f1");
         $('.active').each(function(event) {
           $('.active').empty();
           $(this).removeClass('active');
+          $(this).append($(this).attr('name'));
         });
         $src.addClass('active');
     }
@@ -18,23 +19,40 @@ $('.display').click(function(event) {
 $('.display').click(function(event) {
     // get url from id...still having trouble clearing and picking new url each time
     let $url = $('li.active').attr('id')
-    console.log($url);
-    $info = ($('<ul id="details">'))
+    let $info = ($('<ul id="details">'))
+    let $list = $("<ul>");
+    var $keys;
+    var $values;
+
+    var $innerKeys;
+    var $innerValues;
 
     $.ajax({
         type: 'GET',
         url: $url,
         success: function(result) {
-
             $keys = Object.keys(result);
             $values = Object.values(result);
-            for(var i = 0; i < $keys.length; i++) {
-                $info.append("<li>" + $keys[i] + ": \t" +  $values[i] + '</li>');
-            }
-            $('li.active').empty();
-            $('li.active').append($info);
-        }
+    //print names for each url link
+    for(var i = 0; i < $values.length; i++) {
+        if(typeof $values[i] == "object") {
+            console.log($keys[i]);
+            console.log($values[i])
+            .
+            // for( var j = 0; j < $values[i].length; j++) {
+            //     $list.append($('<li><a href=\'' + $values[i][j] +
+            //     '/>' + ))
+            // }
+
+        } else {
+            console.log('else');
+            $info.append("<li>" + $keys[i] + ":   " +  $values[i] + '</li>');
+      }
+      }
+      }
     })
+    $('li.active').empty();
+    $('li.active').append($info);
 });
 
 //click button to search
@@ -66,6 +84,7 @@ $('#getInfo').click(function(event) {
                     $bigbox.append($cat);
                 } else {
                     $cat.append($result.results[i].name);
+                    $cat.attr('name', $result.results[i].name);
                     $cat.attr("id", $result.results[i].url);
                     $($cat).on('click', function() {
                       $($cat).removeClass('active');
@@ -75,6 +94,7 @@ $('#getInfo').click(function(event) {
                     $bigbox.append($cat);
                 }
             }
+            $('.display').empty();
             $('.display').append($bigbox);
             }
 })
